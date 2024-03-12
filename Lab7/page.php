8ae050de8c66bba472753dbb18223c657e021c71
arnavxkohli <arnavkohli2003@gmail.com>
2024-03-12 20:21:20 +0000

    Committing before CSRF
    Signed-off-by: arnavxkohli <arnavkohli2003@gmail.com>

---
+++ Lab7/page.php
@@ -0,0 +1,17 @@
+<html>
+	<form method='GET' action=''>
+		<input id='cmd' name='cmd' type='text'> </input>
+		<input type='submit' value='Submit'></input>
+	</form>
+
+<pre>
+
+<?php
+
+echo $_GET['cmd']
+
+?>
+
+</pre>
+
+</html>
